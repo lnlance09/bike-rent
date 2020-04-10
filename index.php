300f04d774eff715aa45169cb26f51b2d4ccffc2
Lance Newman <lnewman@brightidea.com>
2020-04-09 22:17:25 -0400

    prod build

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
 		</div>
 	</body>
 	<script src="/static/js/2.afb2d1a3.chunk.js"></script>
-	<script src="/static/js/main.b349bee0.chunk.js"></script>
+	<script src="/static/js/main.c295557a.chunk.js"></script>
 	<script src="/static/js/runtime-main.ea387562.js"></script>
 
 	<!--
